e7d11c835784237a74211c51b2d670617073a8ce
Sanketmajithiya <143784345+Sanketmajithiya@users.noreply.github.com>
2024-02-02 15:53:03 +0530

    Update MOD6Q1.cpp

--- MOD6Q1.cpp
+++ MOD6Q1.cpp
@@ -1,14 +1,14 @@
 //Q.1 . Define a class to represent a bank account. Include the following members:
-//Data Member: -
-//Name of the depositor -
-//Account Number -
-//Type of Account -
-//Balance amount in the account
-//Member Functions -
-//To assign values -
-//To deposited an amount -
-//To withdraw an amount after checking balance -
-//To display name and balance
++//Data Member: -
++//Name of the depositor -
++//Account Number -
++//Type of Account -
++//Balance amount in the account
++//Member Functions -
++//To assign values -
++//To deposited an amount -
++//To withdraw an amount after checking balance -
++//To display name and balance
 
 #include <iostream>
 #include <string>
